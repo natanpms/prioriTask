5e53898f5ad30f6c5d396faf92709c01249dc871
natanpms <natanportoms@outlook.com>
2025-05-28 22:01:58 -0300

    feat: criando opções menus

---
+++ resources/js/components/app-sidebar.tsx
@@ -0,0 +1,58 @@
+import { NavFooter } from '@/components/nav-footer';
+import { NavMain } from '@/components/nav-main';
+import { NavUser } from '@/components/nav-user';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/react';
+import { BookOpen, Folder, ChartNoAxesCombined, NotebookPen, ListTodo, UserPen } from 'lucide-react';
+import AppLogo from './app-logo';
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+        icon: ChartNoAxesCombined,
+    },
+    {
+        title: 'Concentração',
+        href: '/focus',
+        icon: UserPen,
+
+    },
+    {
+        title: 'Minhas Tasks',
+        href: '/tasks',
+        icon: ListTodo,
+    },
+    {
+        title: 'Categorias',
+        href: '/categories',
+        icon: NotebookPen,
+    }
+];
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon" variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href="/dashboard" prefetch>
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarContent>
+                <NavMain items={mainNavItems} />
+            </SidebarContent>
+
+            <SidebarFooter>
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
+}
