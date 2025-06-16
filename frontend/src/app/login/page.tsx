40fc80b7021d2a6f54f6b1372475b392d320530e
natanpms <natanportoms@outlook.com>
2025-06-15 23:00:17 -0300

    fix: corrigindo projeto api

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,11 @@
 function Login() {
     return ( 
-        <div className="underline">
-            hello login
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="bg-red-500 rounded-md shadow-md space-y-2 p-6 h-auto text-center">
+                teste
+            </div>  
         </div>
-     );
+    );
 }
 
 export default Login;
