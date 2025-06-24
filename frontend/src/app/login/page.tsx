5f841f3f9f23634e2b7db7363c7154007c4f579a
natanpms <natanportoms@outlook.com>
2025-06-24 15:52:48 -0300

    feat: cardAuth e telas de autenticação

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,10 +1,8 @@
-function Login() {
-    return ( 
-        <div className="flex justify-center items-center min-h-screen">
-            <div className="bg-red-500 rounded-md shadow-md space-y-2 p-6 h-auto text-center">
-                teste
-            </div>  
-        </div>
+import CardAuth from "@/components/auth/CardAuth";
+
+const Login: React.FC = () => {
+    return (
+       <CardAuth isLogin={true}/>
     );
 }
 
---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import CardAuth from "@/components/auth/CardAuth";
+
+const Register: React.FC = () => {
+    return (
+       <CardAuth isLogin={false}/>
+    );
+}
+
+export default Register;
