da1a0755634d62e624f3e230b59fa40fc49c00e6
Natan Porto Machado da Silva <natanportoms@outlook.com>
2025-11-23 22:57:56 -0300

    fix: erro lint

--- resources/js/components/ui/sidebar.tsx
+++ resources/js/components/ui/sidebar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
-import { CircleChevronLeft, CircleChevronRight, PanelLeftIcon } from "lucide-react"
+import { CircleChevronLeft, CircleChevronRight } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -253,7 +253,7 @@ function SidebarTrigger({
   onClick,
   ...props
 }: React.ComponentProps<typeof Button>) {
-  const { toggleSidebar , open, openMobile} = useSidebar()
+  const { toggleSidebar , open } = useSidebar()
 
   return (
     <Button
